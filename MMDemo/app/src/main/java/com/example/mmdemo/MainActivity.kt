40ce6ab1d492abe35c57cbec0faceb5232335eab
mumu <617958070@qq.com>
2025-07-04 18:44:07 +0800

    add: 初始布局

--- MMDemo/app/src/main/java/com/example/mmdemo/MainActivity.kt
+++ MMDemo/app/src/main/java/com/example/mmdemo/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mmdemo.ui.theme.MMDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,16 +39,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+
+    Column(modifier = modifier, verticalArrangement = Arrangement.Center) {
+        Text(
+            text = "Hello your name: $name!",
+            fontSize = 50.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center,
+            modifier = modifier,
+        )
+        Text(
+            text = "from me",
+            fontSize = 36.sp,
+            textAlign = TextAlign.End,
+            modifier = modifier.align(alignment = Alignment.End)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MMDemoTheme {
-        Greeting("Android")
+        Greeting("Happy")
     }
 }
